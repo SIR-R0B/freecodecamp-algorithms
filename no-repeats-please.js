f25ab36f4daccd604c38a5eca14346e1747fe557
Rob Lyon <sir.rob.lyon@gmail.com>
2019-02-27 21:08:31 +0800

    Create no-repeats-please.js
    function permAlone(str) {
    //convert string to array
    let arr = str.split('');
    let result = 0;
    
    function swap(index1, index2){
    //[a,b,c] swap(1,2) => [a,c,b]
    let temp = arr[index1];
    arr[index1] = arr[index2];
    arr[index2] = temp;
    }
    
    function generate(n){
    let regex = /([a-z])\1+/;
    if ((n === 1) && !(regex.test(arr.join('')))) {
    result++;
    }
    else {
    for (let i = 0; i < n; i++){
    generate(n-1);
    swap((n % 2  ? i : 0), n-1);
    }
    
    }
    
    }
    
    generate(arr.length);
    console.log(result);
      return result;
    }
    
    permAlone('aab');

---
+++ no-repeats-please.js
@@ -0,0 +1,33 @@
+function permAlone(str) {
+//convert string to array
+let arr = str.split('');
+let result = 0;
+
+function swap(index1, index2){
+//[a,b,c] swap(1,2) => [a,c,b]
+let temp = arr[index1];
+arr[index1] = arr[index2];
+arr[index2] = temp;
+}
+
+function generate(n){
+let regex = /([a-z])\1+/;
+if ((n === 1) && !(regex.test(arr.join('')))) {
+result++;
+}
+else {
+for (let i = 0; i < n; i++){
+generate(n-1);
+swap((n % 2  ? i : 0), n-1);
+}
+
+}
+
+}
+
+generate(arr.length);
+console.log(result);
+  return result;
+}
+
+permAlone('aab');
